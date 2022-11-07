18bbf9865c4ba725b135006f068eae0d016d11f7
marionc22 <103053810+marionc22@users.noreply.github.com>
2022-11-07 11:08:14 +0100

    Add files via upload

---
+++ IndianaJones.java
@@ -0,0 +1,13 @@
+public class IndianaJones {
+    public static void main(String[] args) {
+        String movieTitle = "Indiana Jones and the Last Crusade";
+        Boolean watched = true;
+        int releaseYear = 1989;
+        float rating = (float) 8.2;
+
+        System.out.println( movieTitle);
+        System.out.println( watched);
+        System.out.println( releaseYear);
+        System.out.println( rating);
+    }
+}
